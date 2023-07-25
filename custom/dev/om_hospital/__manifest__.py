f47c2e1a093e1f4f36d92995b1ca74c1c978c56c
MohammedFarzin <mohammedfarzin.m123@gmail.com>
2023-07-25 10:27:45 +0530

    first commit

---
+++ custom/dev/om_hospital/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Hospital Management System',
+    'author': 'author_kilian',
+    'website': 'www.hospital.com',
+    'summary': 'This is a module to store the datas of patients',
+    'depends': ['mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient.xml'
+    ]
+}
---
+++ custom/dev/om_hospital/models/patient.py
@@ -0,0 +1,43 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class HospitalPatient(models.Model):
+    _name = 'hospital.patient'
+    _inherit = 'mail.thread'
+    _description = 'Patient Records'
+
+    name = fields.Char(string='Name', required=True, tracking=True)
+    age = fields.Integer(string='Age', tracking=True)
+    is_child = fields.Boolean(string='Is Child ?', tracking=True)
+    notes = fields.Text(string='Notes')
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string='Gender', tracking=True)
+    capitilized_name = fields.Char(string='Capitalized name', compute='_compute_capitilized_name', store=True)
+
+    @api.model_create_multi
+    def create(self, val_list):
+        for record in val_list:
+            record['gender'] = 'female'
+        return super(HospitalPatient, self).create(val_list)
+
+    @api.constrains('is_child', 'age')
+    def _check_child_age(self):
+        for record in self:
+            if self.is_child and self.age == 0:
+                raise ValidationError(_('Age has to be recorded'))
+
+    @api.depends('name')
+    def _compute_capitilized_name(self):
+        for record in self:
+            if record.name:
+                record.capitilized_name = record.name.upper()
+            else:
+                record.capitilized_name = ''
+
+    @api.onchange('age')
+    def _onchange_age(self):
+        if self.age <= 10:
+            self.is_child = True
+        else:
+            self.is_child = False
+
